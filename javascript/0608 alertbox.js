dfdf6875e21a49b3dfc0190e00c86e6740756d1d
shinyelim <cac567@naver.com>
2023-06-08 16:48:59 +0900

    ✨ alert 창 기본구현

---
+++ javascript/0608 alertbox.js
@@ -0,0 +1,10 @@
+document.getElementById('open-btn').addEventListener('click',function(){
+  document.getElementById('alert').style.display='flex;'
+})
+
+function closealert(){
+  document.getElementById('alert').style.display='none';
+
+}
+
+//document.getElementById('close-btn'),addEventListener('click, closealert');
